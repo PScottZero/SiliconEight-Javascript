81108370bc2e777b6471d92ff10c54e13dae1824
PScottZero <8pscott@gmail.com>
2019-01-12 17:22:20 -0500

    12 Jan 2019
    working better?

--- js/chip8.js
+++ js/chip8.js
@@ -6,40 +6,34 @@ class Chip8 {
         this.delay = 0;
         this.sound = 0;
         this.currentKey = -1;
+        this.isRunning = false;
 
-        this.memory = [];
-        this.memory.length = 4096;
+        this.memBuffer = new ArrayBuffer(0x1000);
+        this.memory = new Uint8Array(this.memBuffer);
         this.PC = 0x200;
 
-        this.stack = [];
-        this.stack.length = 16;
+        this.stackBuffer = new ArrayBuffer(0x20);
+        this.stack = new Uint16Array(this.stackBuffer);
         this.SP = -1;
 
-        this.display = [];
-        this.display.length = 2048;
+        this.dispBuffer = new ArrayBuffer(0x800);
+        this.display = new Uint8Array(this.dispBuffer);
         this.height = 32;
         this.width = 64;
 
-        this.key = [];
-        this.key.length = 16;
+        this.keyBuffer = new ArrayBuffer(0x10);
+        this.key = new Uint8Array(this.keyBuffer);
 
-        this.V = [];
-        this.V.length = 16;
+        this.regBuffer = new ArrayBuffer(0x10);
+        this.V = new Uint8Array(this.regBuffer);
+    }
 
-        for (let i = 0; i < this.memory.length; i++)
-            this.memory[i] = 0x0;
-
-        for (let i = 0; i < this.display.length; i++)
-            this.display[i] = 0x0;
-
-        for (let i = 0; i < this.stack.length; i++) {
-            this.stack[i] = 0x0;
-            this.key[i] = 0x0;
-            this.V[i] = 0x0;
-        }
+    state() {
+        return this.isRunning;
     }
 
     load(chip8) {
+        this.isRunning = false;
         let file = document.getElementById("file_selector").files[0];
         let reader = new FileReader();
         reader.onload = function() {
@@ -47,25 +41,25 @@ class Chip8 {
             for (let i = 0; i < result.length; i++) {
                 chip8.memory[i + chip8.PC] = result[i].charCodeAt(0);
             }
-            chip8.loadFont(chip8);
+            chip8.loadFont();
         };
         reader.readAsBinaryString(file);
-        chip8.isRunning = true;
+        this.isRunning = true;
     }
 
-    keypress(chip8, key, value) {
-        chip8.key[key] = value;
-        chip8.currentKey = key;
+    keypress(key, value) {
+        this.key[key] = value;
+        this.currentKey = key;
     }
 
-    step(chip8) {
-        chip8.opcode = (chip8.memory[chip8.PC] << 8) | (chip8.memory[chip8.PC + 1]);
-        chip8.decode(chip8);
-        chip8.delay--;
-        chip8.sound--;
+    step() {
+        this.opcode = (this.memory[this.PC] << 8) | (this.memory[this.PC + 1]);
+        this.decode();
+        if (this.delay > 0) this.delay--;
+        if (this.sound > 0) this.sound--;
     }
 
-    loadFont(chip8) {
+    loadFont() {
         let font = Array(
             0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
             0x20, 0x60, 0x20, 0x20, 0x70, // 1
@@ -85,150 +79,145 @@ class Chip8 {
             0xF0, 0x80, 0xF0, 0x80, 0x80  // F
         );
 
-        for (let i = 0; i < 0x50; i++) chip8.memory[i] = font[i];
+        for (let i = 0; i < 0x50; i++) this.memory[i] = font[i];
     }
 
-    async decode(chip8) {
+    decode() {
 
-        let reg1 = (chip8.opcode >> 8) & 0xF;
-        let reg2 = (chip8.opcode >> 4) & 0xF;
-        let lo = chip8.opcode & 0xFF;
-        let subop = chip8.opcode & 0xF;
+        let reg1 = (this.opcode >> 8) & 0xF;
+        let reg2 = (this.opcode >> 4) & 0xF;
+        let lo = this.opcode & 0xFF;
+        let subop = this.opcode & 0xF;
+        let temp = 0;
 
-        console.log(chip8.opcode.toString(16));
-
-        switch (chip8.opcode & 0xF000) {
+        switch (this.opcode & 0xF000) {
 
             case 0x0000:
                 switch (lo) {
                     case 0xE0:
-                        chip8.clearDisplay(chip8);
+                        this.clearDisplay();
                         break;
                     case 0xEE:
-                        chip8.PC = chip8.stack[chip8.SP];
-                        chip8.SP--;
+                        this.PC = this.stack[this.SP];
+                        this.SP--;
                         break;
                 }
                 break;
 
             case 0x1000:
-                chip8.PC = chip8.opcode & 0xFFF;
-                chip8.PC -= 2;
+                this.PC = this.opcode & 0xFFF;
+                this.PC -= 2;
                 break;
 
             case 0x2000:
-                chip8.SP++;
-                chip8.stack[chip8.SP] = chip8.PC;
-                chip8.PC = chip8.opcode & 0xFFF;
-                chip8.PC -= 2;
+                this.SP++;
+                this.stack[this.SP] = this.PC;
+                this.PC = this.opcode & 0xFFF;
+                this.PC -= 2;
                 break;
 
             case 0x3000:
-                if (chip8.V[reg1] === lo) chip8.PC += 2;
+                if (this.V[reg1] === lo) this.PC += 2;
                 break;
 
             case 0x4000:
-                if (chip8.V[reg1] !== lo) chip8.PC += 2;
+                if (this.V[reg1] !== lo) this.PC += 2;
                 break;
 
             case 0x5000:
-                if (chip8.V[reg1] === chip8.V[reg2]) chip8.PC += 2;
+                if (this.V[reg1] === this.V[reg2]) this.PC += 2;
                 break;
 
             case 0x6000:
-                chip8.V[reg1] = lo;
+                this.V[reg1] = lo;
                 break;
 
             case 0x7000:
-                chip8.V[reg1] += lo;
-                if (chip8.V[reg1] > 0xFF) chip8.V[reg1] -= 0x100;
+                this.V[reg1] += lo;
                 break;
             case 0x8000:
                 switch (subop) {
 
                     case 0x0:
-                        chip8.V[reg1] = chip8.V[reg2];
+                        this.V[reg1] = this.V[reg2];
                         break;
 
                     case 0x1:
-                        chip8.V[reg1] = chip8.V[reg1] | chip8.V[reg1];
+                        this.V[reg1] = this.V[reg1] | this.V[reg1];
                         break;
 
                     case 0x2:
-                        chip8.V[reg1] = chip8.V[reg1] & chip8.V[reg2];
+                        this.V[reg1] = this.V[reg1] & this.V[reg2];
                         break;
 
                     case 0x3:
-                        chip8.V[reg1] = chip8.V[reg1] ^ chip8.V[reg2];
+                        this.V[reg1] = this.V[reg1] ^ this.V[reg2];
                         break;
 
                     case 0x4:
-                        chip8.V[reg1] += chip8.V[reg2];
-                        if (chip8.V[reg1] > 0xFF) {
-                            chip8.V[0xF] = 1;
-                            chip8.V[reg1] -= 0x100;
-                        } else chip8.V[0xF] = 0;
+                        temp = this.V[reg1] + this.V[reg2];
+                        if (temp > 0xFF) this.V[0xF] = 1;
+                        else this.V[0xF] = 0;
+                        this.V[reg1] = temp;
                         break;
 
                     case 0x5:
-                        chip8.V[reg1] -= chip8.V[reg2];
-                        if (chip8.V[reg1] < 0) {
-                            chip8.V[0xF] = 0;
-                            chip8.V[reg1] += 0x100;
-                        } else chip8.V[0xF] = 1;
+                        temp = this.V[reg1] - this.V[reg2];
+                        if (temp < 0) this.V[0xF] = 0;
+                        else this.V[0xF] = 1;
+                        this.V[reg1] = temp;
                         break;
 
                     case 0x6:
-                        chip8.V[0xF] = chip8.V[reg1] & 0x1;
-                        chip8.V[reg1] >>= 1;
+                        this.V[0xF] = this.V[reg1] & 0x1;
+                        this.V[reg1] >>= 1;
                         break;
 
                     case 0x7:
-                        chip8.V[reg1] = chip8.V[reg2] - chip8.V[reg1];
-                        if (chip8.V[reg1] < 0) {
-                            chip8.V[0xF] = 0;
-                            chip8.V[reg1] += 0x100;
-                        } else chip8.V[0xF] = 1;
+                        temp = this.V[reg2] - this.V[reg1];
+                        if (temp < 0) this.V[0xF] = 0;
+                        else this.V[0xF] = 1;
+                        this.V[reg1] = temp;
                         break;
 
                     case 0xE:
-                        chip8.V[0xF] = (chip8.V[reg1] >> 7) & 0x1;
-                        chip8.V[reg1] <<= 1;
+                        this.V[0xF] = (this.V[reg1] >> 7) & 0x1;
+                        this.V[reg1] <<= 1;
                         break;
                 }
                 break;
 
             case 0x9000:
-                if (chip8.V[reg1] !== chip8.V[reg2]) chip8.PC += 2;
+                if (this.V[reg1] !== this.V[reg2]) this.PC += 2;
                 break;
 
             case 0xA000:
-                chip8.I = chip8.opcode & 0xFFF;
+                this.I = this.opcode & 0xFFF;
                 break;
 
             case 0xB000:
-                chip8.PC = chip8.V[0x0] + chip8.opcode;
+                this.PC = this.V[0x0] + this.opcode;
                 break;
 
             case 0xC000:
-                chip8.V[reg1] = Math.floor(Math.random() * 0x100) & lo;
+                this.V[reg1] = Math.floor(Math.random() * 0x100) & lo;
                 break;
 
             case 0xD000:
-                let x = chip8.V[reg1];
-                let y = chip8.V[reg2];
-                chip8.V[0xF] = 0;
+                let x = this.V[reg1];
+                let y = this.V[reg2];
+                this.V[0xF] = 0;
 
                 for (let r = 0; r < subop; r++) {
                     let bitOffset = 7;
                     for (let c = 0; c < 8; c++) {
-                        let flip = chip8.setPixel(chip8, y + r, x + c,
-                            (chip8.memory[chip8.I + r] >> bitOffset) & 0x1);
-                        chip8.V[0xF] = chip8.V[0xF] | flip;
+                        let flip = this.setPixel(y + r, x + c,
+                            (this.memory[this.I + r] >> bitOffset) & 0x1);
+                        this.V[0xF] = this.V[0xF] | flip;
                         bitOffset--;
                     }
                 }
-                chip8.draw(chip8);
+                this.draw();
                 break;
 
             case 0xE000:
@@ -236,11 +225,11 @@ class Chip8 {
                 switch (lo) {
 
                     case 0x9E:
-                        if (chip8.key[chip8.V[reg1]] === 1) chip8.PC += 2;
+                        if (this.key[this.V[reg1]] === 1) this.PC += 2;
                         break;
 
                     case 0xA1:
-                        if (chip8.key[chip8.V[reg1]] === 0) chip8.PC += 2;
+                        if (this.key[this.V[reg1]] === 0) this.PC += 2;
                         break;
                 }
                 break;
@@ -250,153 +239,159 @@ class Chip8 {
                 switch (lo) {
 
                     case 0x07:
-                        chip8.V[reg1] = chip8.delay;
+                        this.V[reg1] = this.delay;
                         break;
 
                     case 0x0A:
-                        chip8.currentKey = -1;
-                        while (chip8.currentKey < 0) {
-                            await new Promise(resolve => setTimeout(resolve, 2));
+                        this.currentKey = -1;
+                        this.isRunning = false;
+                        while (this.currentKey < 0) {
+                            this.sleep(10);
                         }
-                        chip8.V[reg1] = chip8.currentKey;
+                        this.V[reg1] = this.currentKey;
+                        this.isRunning = true;
                         break;
 
                     case 0x15:
-                        chip8.delay = chip8.V[reg1];
+                        this.delay = this.V[reg1];
                         break;
 
                     case 0x18:
-                        chip8.sound = chip8.V[reg1];
+                        this.sound = this.V[reg1];
                         break;
 
                     case 0x1E:
-                        chip8.I += chip8.V[reg1];
+                        this.I += this.V[reg1];
                         break;
 
                     case 0x29:
-                        switch (chip8.V[reg1]) {
+                        switch (this.V[reg1]) {
                             case 0x0:
-                                chip8.I = 0x00;
+                                this.I = 0x00;
                                 break;
                             case 0x1:
-                                chip8.I = 0x05;
+                                this.I = 0x05;
                                 break;
                             case 0x2:
-                                chip8.I = 0x0A;
+                                this.I = 0x0A;
                                 break;
                             case 0x3:
-                                chip8.I = 0x0F;
+                                this.I = 0x0F;
                                 break;
                             case 0x4:
-                                chip8.I = 0x14;
+                                this.I = 0x14;
                                 break;
                             case 0x5:
-                                chip8.I = 0x19;
+                                this.I = 0x19;
                                 break;
                             case 0x6:
-                                chip8.I = 0x1E;
+                                this.I = 0x1E;
                                 break;
                             case 0x7:
-                                chip8.I = 0x23;
+                                this.I = 0x23;
                                 break;
                             case 0x8:
-                                chip8.I = 0x28;
+                                this.I = 0x28;
                                 break;
                             case 0x9:
-                                chip8.I = 0x2D;
+                                this.I = 0x2D;
                                 break;
                             case 0xA:
-                                chip8.I = 0x32;
+                                this.I = 0x32;
                                 break;
                             case 0xB:
-                                chip8.I = 0x37;
+                                this.I = 0x37;
                                 break;
                             case 0xC:
-                                chip8.I = 0x3C;
+                                this.I = 0x3C;
                                 break;
                             case 0xD:
-                                chip8.I = 0x41;
+                                this.I = 0x41;
                                 break;
                             case 0xE:
-                                chip8.I = 0x46;
+                                this.I = 0x46;
                                 break;
                             case 0xF:
-                                chip8.I = 0x4B;
+                                this.I = 0x4B;
                                 break;
                         }
                         break;
 
                     case 0x33:
-                        let regVal = chip8.V[reg1];
+                        let regVal = this.V[reg1];
                         let bcd1 = regVal % 10;
                         regVal /= 10;
                         let bcd2 = regVal % 10;
                         let bcd3 = regVal / 10;
-                        chip8.memory[chip8.I] = bcd3;
-                        chip8.memory[chip8.I + 1] = bcd2;
-                        chip8.memory[chip8.I + 2] = bcd1;
+                        this.memory[this.I] = bcd3;
+                        this.memory[this.I + 1] = bcd2;
+                        this.memory[this.I + 2] = bcd1;
                         break;
 
                     case 0x55:
                         for (let r = 0; r <= reg1; r++)
-                            chip8.memory[chip8.I + r] = chip8.V[r];
+                            this.memory[this.I + r] = this.V[r];
                         break;
 
                     case 0x65:
                         for (let r = 0; r <= reg1; r++)
-                            chip8.V[r] = chip8.memory[chip8.I + r];
+                            this.V[r] = this.memory[this.I + r];
                         break;
                 }
                 break;
         }
-        chip8.PC += 2;
+        this.PC += 2;
     }
 
-    clear(chip8) {
-        chip8.opcode = 0x0;
-        chip8.I = 0x0;
-        chip8.delay = 0;
-        chip8.sound = 0;
-        chip8.PC = 0x200;
-        chip8.SP = -1;
-        chip8.clearDisplay(chip8);
+    clear() {
+        this.opcode = 0x0;
+        this.I = 0x0;
+        this.delay = 0;
+        this.sound = 0;
+        this.PC = 0x200;
+        this.SP = -1;
+        this.clearDisplay();
 
-        for (let i = 0; i < chip8.memory.length; i++)
-            chip8.memory[i] = 0x0;
+        for (let i = 0; i < this.memory.length; i++)
+            this.memory[i] = 0x0;
 
-        for (let i = 0; i < chip8.display.length; i++)
-            chip8.display[i] = 0x0;
+        for (let i = 0; i < this.display.length; i++)
+            this.display[i] = 0x0;
 
-        for (let i = 0; i < chip8.stack.length; i++) {
-            chip8.stack[i] = 0x0;
-            chip8.key[i] = 0x0;
-            chip8.V[i] = 0x0;
+        for (let i = 0; i < this.stack.length; i++) {
+            this.stack[i] = 0x0;
+            this.key[i] = 0x0;
+            this.V[i] = 0x0;
         }
     }
 
-    setPixel(chip8, row, col, value) {
-        if (row >= chip8.height || row < 0) row %= chip8.height;
-        if (col >= chip8.width || col < 0) col %= chip8.width;
-        chip8.display[row*64 + col] ^= value;
-        if (chip8.display[row*64 + col] !== value && value === 1) return 1;
+    setPixel(row, col, value) {
+        if (row >= this.height || row < 0) row %= this.height;
+        if (col >= this.width || col < 0) col %= this.width;
+        this.display[row*64 + col] ^= value;
+        if (this.display[row*64 + col] !== value && value === 1) return 1;
         return 0;
     }
 
-    clearDisplay(chip8) {
-        for (let i = 0; i < chip8.display.length; i++)
-            chip8.display[i] = 0;
+    clearDisplay() {
+        for (let i = 0; i < this.display.length; i++)
+            this.display[i] = 0;
     }
 
-    draw(chip8) {
+    draw() {
         let canvas = document.getElementById("chip8");
         let ctx = canvas.getContext("2d");
         ctx.beginPath();
-        for (let i = 0; i < chip8.display.length; i++) {
-            if (chip8.display[i] === 1)
+        for (let i = 0; i < this.display.length; i++) {
+            if (this.display[i] === 1)
                 ctx.fillStyle = "#4fff95";
             else
                 ctx.fillStyle = "#000";
             ctx.fillRect((i % 64) * 5, Math.floor(i / 64) * 5, 5, 5);
         }
+    }
+
+    sleep(time_ms) {
+        return new Promise(resolve => setTimeout(resolve, time_ms))
     }
 }
--- js/scripts.js
+++ js/scripts.js
@@ -1,63 +1,63 @@
 $(document).ready(function(){
     let chip8 = new Chip8();
-    $("#file_selector").on('change', function() {
-        chip8.clear(chip8);
+    $("#file_selector").on('change', async function () {
+        chip8.clear();
         chip8.load(chip8);
         window.setInterval(function() {
-            chip8.step(chip8);
-        }, 1);
+            if (chip8.state()) chip8.step();
+        }, 2);
     });
 
     document.onkeydown = function (e) {
         e = e || window.event;
         switch (e.keyCode) {
             case 49:
-                chip8.keypress(chip8, 0x1, 1);
+                chip8.keypress(0x1, 1);
                 break;
             case 50:
-                chip8.keypress(chip8, 0x2, 1);
+                chip8.keypress(0x2, 1);
                 break;
             case 51:
-                chip8.keypress(chip8, 0x3, 1);
+                chip8.keypress(0x3, 1);
                 break;
             case 52:
-                chip8.keypress(chip8, 0xC, 1);
+                chip8.keypress(0xC, 1);
                 break;
             case 81:
-                chip8.keypress(chip8, 0x4, 1);
+                chip8.keypress(0x4, 1);
                 break;
             case 87:
-                chip8.keypress(chip8, 0x5, 1);
+                chip8.keypress(0x5, 1);
                 break;
             case 69:
-                chip8.keypress(chip8, 0x6, 1);
+                chip8.keypress(0x6, 1);
                 break;
             case 82:
-                chip8.keypress(chip8, 0xD, 1);
+                chip8.keypress(0xD, 1);
                 break;
             case 65:
-                chip8.keypress(chip8, 0x7, 1);
+                chip8.keypress(0x7, 1);
                 break;
             case 83:
-                chip8.keypress(chip8, 0x8, 1);
+                chip8.keypress(0x8, 1);
                 break;
             case 68:
-                chip8.keypress(chip8, 0x9, 1);
+                chip8.keypress(0x9, 1);
                 break;
             case 70:
-                chip8.keypress(chip8, 0xE, 1);
+                chip8.keypress(0xE, 1);
                 break;
             case 90:
-                chip8.keypress(chip8, 0xA, 1);
+                chip8.keypress(0xA, 1);
                 break;
             case 88:
-                chip8.keypress(chip8, 0x0, 1);
+                chip8.keypress(0x0, 1);
                 break;
             case 67:
-                chip8.keypress(chip8, 0xB, 1);
+                chip8.keypress(0xB, 1);
                 break;
             case 86:
-                chip8.keypress(chip8, 0xF, 1);
+                chip8.keypress(0xF, 1);
                 break;
         }
     };
@@ -66,52 +66,52 @@ $(document).ready(function(){
         e = e || window.event;
         switch (e.keyCode) {
             case 49:
-                chip8.keypress(chip8, "1", 0);
+                chip8.keypress(0x1, 0);
                 break;
             case 50:
-                chip8.keypress(chip8, "2", 0);
+                chip8.keypress(0x2, 0);
                 break;
             case 51:
-                chip8.keypress(chip8, "3", 0);
+                chip8.keypress(0x3, 0);
                 break;
             case 52:
-                chip8.keypress(chip8, "C", 0);
+                chip8.keypress(0xC, 0);
                 break;
             case 81:
-                chip8.keypress(chip8, "4", 0);
+                chip8.keypress(0x4, 0);
                 break;
             case 87:
-                chip8.keypress(chip8, "5", 0);
+                chip8.keypress(0x5, 0);
                 break;
             case 69:
-                chip8.keypress(chip8, "6", 0);
+                chip8.keypress(0x6, 0);
                 break;
             case 82:
-                chip8.keypress(chip8, "D", 0);
+                chip8.keypress(0xD, 0);
                 break;
             case 65:
-                chip8.keypress(chip8, "7", 0);
+                chip8.keypress(0x7, 0);
                 break;
             case 83:
-                chip8.keypress(chip8, "8", 0);
+                chip8.keypress(0x8, 0);
                 break;
             case 68:
-                chip8.keypress(chip8, "9", 0);
+                chip8.keypress(0x9, 0);
                 break;
             case 70:
-                chip8.keypress(chip8, "E", 0);
+                chip8.keypress(0xE, 0);
                 break;
             case 90:
-                chip8.keypress(chip8, "A", 0);
+                chip8.keypress(0xA, 0);
                 break;
             case 88:
-                chip8.keypress(chip8, "0", 0);
+                chip8.keypress(0x0, 0);
                 break;
             case 67:
-                chip8.keypress(chip8, "B", 0);
+                chip8.keypress(0xB, 0);
                 break;
             case 86:
-                chip8.keypress(chip8, "F", 0);
+                chip8.keypress(0xF, 0);
                 break;
         }
     };
